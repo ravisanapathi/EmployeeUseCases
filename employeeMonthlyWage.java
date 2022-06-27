df073cebff197bbca662c0f257c71726c2af2911
ravisanapathi <107634597+ravisanapathi@users.noreply.github.com>
2022-06-27 16:56:24 +0530

    Update employeeMonthlyWage.java

--- employeeMonthlyWage.java
+++ employeeMonthlyWage.java
@@ -2,28 +2,29 @@ package employeeMonthlyWage;
 //use case 5 : employee Monthly wage
 
 public class employeeMonthlyWage{
-	int ISPRESENTFullTime=1;
-		int ISPRESENTPartTime=2;
-		int wagePerHour=20;
-		int noOfWorkingdays=20;
-		int workingHrsFullTime=8;
-		int workingHrsPartTime=4;
+    public static final	int ISPRESENTFullTime=1;
+	public static final	int ISPRESENTPartTime=2;
+	public static final	int wagePerHour=20;
+	public static final	int noOfWorkingdays=20;
+	public static final	int workingHrsFullTime=8;
+	public static final	int workingHrsPartTime=4;
 	public static void main(String[] args) {
-                // Variables
-		int empHrs=0; empWage=0; totalEmpWage=0;
+                // Variables 
+	    int	empWage=0; 
+	    int	totalEmpWage=0;
 		//computation
 		
 		for (int day = 1; day <= noOfWorkingdays; day++) {
 		     int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		
 		if(empCheck==ISPRESENTFullTime) {
-			int empWage=wagePerHour*workingHrsFullTime;
+			 empWage=wagePerHour*workingHrsFullTime;
 			
 			System.out.println("Employee is present FullTime");
 			System.out.println("Employee Wage = "+empWage);
 		}
 		else if(empCheck==ISPRESENTPartTime) {
-			int empWage=wagePerHour*workingHrsPartTime;
+			empWage=wagePerHour*workingHrsPartTime;
 			
 			System.out.println("Employee is present PartTime");
 			System.out.println("Employee Wage = "+empWage);
@@ -35,4 +36,5 @@ public class employeeMonthlyWage{
 			System.out.println("Emp Wage: " + empWage);
 	}
 		       System.out.println("Total Emp Wage: " + totalEmpWage);
+}
 }
